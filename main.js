631e0ca1f0b8a3a0a4cdc1f26200e721059b88d5
johulk <igor1brag@hotmail.com>
2023-04-04 21:43:13 +0100

    Restart : Particles

--- main.js
+++ main.js
@@ -1,20 +1,4 @@
-fetch('projects.json')
-  .then(response => response.json())
-  .then(data => {
-    const projectCards = document.getElementById('project-cards');
-    data.projects.forEach(project => {
-        const card = document.createElement('div');
-        card.classList.add('col-md-3', 'mx-3', 'my-3');
-      card.innerHTML = `
-        <div class="card">
-          <img src="${project.image}" class="card-img-top" alt="${project.title}">
-          <div class="card-body">
-            <h5 class="card-title">${project.title}</h5>
-            <p class="card-text">${project.description}</p>
-            <a href="${project.link}" class="btn btn-primary">Learn More</a>
-          </div>
-        </div>
-      `;
-      projectCards.appendChild(card);
-    });
-  });
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
